64654a95f50dfb96400f56936b079fe942789f5e
Prakhar_sharma <2019uch0014@iitjammu.ac.in>
2021-04-25 13:14:29 +0530

    master volume fixed

---
+++ PlantsVsZombie Codegym/Assets/script/audioSetting.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+public class audioSetting : MonoBehaviour
+{
+    public AudioMixer masterSlider;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    public void MasterVolume(float volume)
+    {
+        masterSlider.SetFloat("Volume", Mathf.Log10(volume) * 20);
+    }
+    void MusicVolume()
+    {
+        Camera.main.GetComponent<AudioSource>().volume = musicSlider.value;
+    }
+}
