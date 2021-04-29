8aa0af58b5b8fa7fd7a7b314e06aad7902e56d44
Prakhar_sharma <2019uch0014@iitjammu.ac.in>
2021-04-29 18:55:28 +0530

    correction

--- PlantsVsZombie Codegym/Assets/gameover.cs
+++ PlantsVsZombie Codegym/Assets/gameover.cs
@@ -8,6 +8,7 @@ public class gameover : MonoBehaviour
     // Update is called once per frame
     public GameObject gameOverPanel;
     public static gameover instance;
+    public GameObject old;
     private void Awake()
     {
         instance = this;
@@ -16,10 +17,25 @@ public class gameover : MonoBehaviour
     {
         Debug.Log("GameOVErCAlLed");
         gameOverPanel.SetActive(true);
+        Time.timeScale = 0f;
+        old.SetActive(false);
+        
 
     }
     public void restart()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Gameplay window");
+    }
+    public void quit()
+    {
+        // save any game data here
+#if UNITY_EDITOR
+        // Application.Quit() does not work in the editor so
+        // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+         Application.Quit();
+#endif
     }
 }
